16c1a826b89c53494e41baf74b43896a62098b01
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-12-12 21:18:57 -0500

    Update qtl_pipeline.sh

--- qtl_pipeline.sh
+++ qtl_pipeline.sh
@@ -6,8 +6,8 @@
 module load R/3.5.1
 
 
-### Uncomment line 10 to run scan1 not in parallel, then comment line 11. Otherwise qsub run_scan1_parallel.sh first,
-#       then qsub this .sh file
+### Uncomment line 10 to run scan1 not in parallel, then comment line 11 and qsub this .sh file. 
+#      Otherwise qsub run_scan1_parallel.sh first, then qsub this .sh file when run_scan1_parallel.sh is finished
 #Rscript scan1_HPC.R attie_islet_284_qtl_viewer_v2.RData dataset.islet.mrna rankz 8
 Rscript gather_scan1_chunks.R islet_mrna_additive_scan 1 22 test.rds
 Rscript find_peaks.R attie_islet_284_qtl_viewer_v2.RData attie_islet_mrna_rZ_qtl_lod.rds dataset.islet.mrna 6 8 additive rankz
