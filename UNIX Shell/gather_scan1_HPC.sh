63a3459c9e0b0ba76bdabd8680c834258c528cc6
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2019-02-26 13:13:21 -0500

    Add files via upload

---
+++ UNIX Shell/gather_scan1_HPC.sh
@@ -0,0 +1,15 @@
+#PBS -l nodes=1:ppn=8
+#PBS -q batch
+#PBS -l walltime=72:00:00
+module load R/3.5.1
+
+
+pattern='esc_proteins_additive_scan'
+chunk_start='1'
+chunk_end='197'
+func='cbind'
+out_file='munger_esc_proteins_additive_qtl_lod'
+
+
+
+Rscript gather_scan1_chunks.R "pattern=$pattern" "chunk_start=$chunk_start" "chunk_end=$chunk_end" "func=$func" "out_file=$out_file
