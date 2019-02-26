35b7efd3b0b31642acf4821aafba761b288418fa
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2019-02-26 11:07:27 -0500

    Add files via upload

--- UNIX Shell/scan1_paralllel_HPC.sh
+++ UNIX Shell/scan1_paralllel_HPC.sh
@@ -4,24 +4,24 @@
 module load R/3.5.1
 
 
+
 job_name='run_esc_prot'
 
-
-for i in {1..250}
+for i in {1..197}
 do
-  echo "#PBS -l nodes=1:ppn=4
+  echo "#PBS -l nodes=1:ppn=8
 #PBS -q batch
 
 module load R/3.5.1
 
 
-viewer_data='prelim_proteomics_correctedIDs_v3.1.RData'
+viewer_data='munger_esc_viewer_174_v3.RData'
 dataset='dataset.esc.proteins'
 num_cores='8'
 type_data='rankz'
-int_name='sex'
+int_name='NA'
 chunk_number=${i}
-chunk_size=32
+chunk_size=40
 
 
 Rscript qtl2_interaction_scan.R viewer_data=\$viewer_data dataset=\$dataset num_cores=\$num_cores type_scan=\$type_scan type_data=\$type_data int_name=\$int_name chunk_number=\$chunk_number chunk_size=\$chunk_size" >> "${job_name}_${i}.sh"
