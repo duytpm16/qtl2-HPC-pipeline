00eb92f4f6193605fb6c511ebf4933f37cc98dc6
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2019-04-19 12:24:52 -0400

    Update qtl2_mediate_QTL_HPC.sh

--- Version 1/qtl2_mediate_QTL_HPC.sh
+++ Version 1/qtl2_mediate_QTL_HPC.sh
@@ -23,9 +23,9 @@
 module load R/3.5.1
 
 
-job_name='munger_esc_pQTL_mediation'
+job_name='attie_islet_pQTL_mediation'
 
-for i in {1..127}
+for i in {1..79}
 do
   echo "#PBS -l nodes=1:ppn=8
 #PBS -q batch
@@ -37,12 +37,12 @@ med_dataset='dataset.islet.mrna.284'
 targ_id='protein_id'
 med_id='gene_id'
 expr_type='rankz'
-type_peak = 'additive'
+type_peak='additive'
 med_method='double-lod-diff'
 z_thres='-4'
 pos_thres='10'
 cores='8'
-filename='attie_islet_proteins_distal_pQTL_mediation'
+filename='attie_islet_pQTL_mediation'
 chunk_number=${i}
 chunk_size='100'
 Rscript mediation_distal_QTL.R viewer_data=\$viewer_data targ_dataset=\$targ_dataset med_dataset=\$med_dataset targ_id=\$targ_id med_id=\$med_id expr_type=\$expr_type type_peak=\$type_peak med_method=\$med_method z_thres=\$z_thres pos_thres=\$pos_thres cores=\$cores filename=\$filename chunk_number=\$chunk_number chunk_size=\$chunk_size" >> "${job_name}_${i}.sh"
