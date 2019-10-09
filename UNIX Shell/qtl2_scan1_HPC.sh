4d99f779fd113232632a11197e64ba95c6fd5e1a
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2019-10-09 13:35:28 -0400

    Update qtl2_scan1_HPC.sh

--- UNIX Shell/qtl2_scan1_HPC.sh
+++ UNIX Shell/qtl2_scan1_HPC.sh
@@ -1,3 +1,41 @@
+####################################################################################################################
+#
+#   This script performs qtl2 additive or interactive scan using R script located:
+#       https://github.com/duytpm16/qtl2-HPC-pipeline/blob/master/R%20scripts/qtl2_scan1.R
+#
+#   Notes*:
+#         The QTL scan can be run in 'chunks' or all at once.
+#             
+#             Example for chunk size/number: 
+#             Suppose there are 5433 phenotype columns. If chunk size is 1000, then there should be 6 different chunk_number,
+#                  with the chunk_number value being 1,2,3,4,5, or 6 to get the columns:
+#                       1-1000,1001-2000,2001-3000,3001-4000,4001-5000,5001-5433, respectively.
+#
+#         If you do not want to run in chunks, make chunk_number and chunk_size 'NA' or 'na'
+#
+#
+#
+#
+#   Input (for shell script; see (https://github.com/duytpm16/qtl2-HPC-pipeline/blob/master/UNIX%20Shell/qtl2_scan1_HPC.sh):
+#      1: viewer:        Path to the qtl viewer .RData 
+#      2: dataset_expr:  Which dataset.* and data type to use. Ex. dataset.islet.proteins|data (if only one expression matrix) or dataset.islet.proteins|data|rz (if there are multiple expression matrices in 'data')
+#      3: int_name:      (Optional) A string with interactive variables in samples dataframe separated by '|'.     Ex. 'sex' or 'sex|batch'. 'NA' or 'na' if not used
+#      4: num_cores:     Number of cores to run
+#      5: chunk_number:  (Optional) Numeric value of the chunk number. 'NA' or 'na' if not used
+#      6: chunk_size:    (Optional) Numeric value of chunk size. Should be consistent. 'NA' or 'na' if not used
+#
+#   Output: 
+#       1: Matrix containing LOD scores for each of the phenotype that was given to scan1 at each marker.
+#
+#
+#
+#
+#   Authors: Duy Pham, Andrew Deighan, & Isabela Gyuricza
+#   Date:    October 31, 2018
+#   E-mails: duy.pham@jax.org, andrew.deighan@jax.org, & isabela.gyuricza@jax.org
+####################################################################################################################
+
+
 #PBS -q batch
 #PBS -l nodes=1:ppn=1
 
